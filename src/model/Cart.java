14146c1776e1d06c68e2ef85e74b09d35a41f3b3
isel tarek <iseltarek15@gmail.com>
2025-07-04 05:23:40 +0300

    Added Cart class.

--- src/model/Cart.java
+++ src/model/Cart.java
@@ -1,2 +1,24 @@
 public class Cart {
+    Private List<CartItem> items;
+
+    Cart(){
+      this.items = new List<CartItem>;
+    }
+
+    public void AddProduct(Product product, int quantity){
+         for (CartItem item : items) {
+            if (item.getProduct().equals(product)) {
+                int newQuantity = item.getQuantity() + quantity;
+                item.setQuantity(newQuantity);
+                return;
+            }
+        }
+        items.add(new CartItem(product, quantity));
+    }
+    public void removeProduct(Product product) {
+        items.removeIf(item -> item.getProduct().equals(product));
+    }
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
 }
