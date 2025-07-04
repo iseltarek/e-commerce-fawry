3d12839527b7031c2c6a174698752c5312366df1
isel tarek <iseltarek15@gmail.com>
2025-07-04 05:15:18 +0300

    Added CartItem Class.

--- src/model/CartItem.java
+++ src/model/CartItem.java
@@ -1,2 +1,27 @@
 public class CartItem {
+    private Product product;
+    private Int quantity;
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        if (quantity > 0) {
+            this.quantity = quantity;
+        }
+    }
+
+     public Product getProduct() {
+        return product;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        if (quantity > 0) {
+            this.quantity = quantity;
+        }
+    }
+    public double getTotalPrice() {
+        return product.getPrice() * quantity;
+    }
+
 }
